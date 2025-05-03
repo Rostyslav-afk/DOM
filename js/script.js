d55c612f1d3030a07cf166163a37a0bcfbf1b5c9
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-05-03 15:08:25 +0200

    BOM
    only practice

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// BOM
+// console.log(navigator.userAgent); // Браузер
+
+// if (navigator.userAgent.includes("Chrome")) {
+//     console.log("В користувача скачаний Chrome");
+// } else if (navigator.userAgent.includes("Firefox")) {
+//     console.log("В користувача скачаний Firefox");
+// }
+
+// //Платформа
+// console.log(navigator.platform);
+
+// // URl
+// console.log(location.href);
+
+// //Міняємо URl
+// location.href = "https:/google.com"
+
+// Історія Браузера
+// history.back();
+// history.forward();
